19a50c823a69e3421cd50d0eb3610df6435c38fc
jholz932 <Jeremias.Holzingersm@gmail.com>
2026-01-19 08:43:12 +0100

    Update Program.cs

--- FriendsOfAward/Program.cs
+++ FriendsOfAward/Program.cs
@@ -1,22 +1,36 @@
 using FriendsOfAward.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddHttpContextAccessor();
+
+// Register a distributed cache (in-memory for single-server dev)
+builder.Services.AddDistributedMemoryCache();
+
+// Register session after the cache
+builder.Services.AddSession();
 
 var app = builder.Build();
+
+app.UseSession(); // enable session middleware
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
