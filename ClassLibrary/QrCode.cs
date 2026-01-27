01f4467da908eef534241db5b3733d2723f5faa6
jholz932 <Jeremias.Holzingersm@gmail.com>
2026-01-27 12:20:40 +0100

    Refactor DbClass in einzelne Klassen. Einzahl-/Mehrzahl-Benennungen richtig gestellt.

---
+++ ClassLibrary/QrCode.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public class QrCode
+    {
+        public string QrId { get; set; }
+
+        public QrCode(string qrId)
+        {
+            QrId = qrId;
+        }
+
+        public static bool SaveQRtoDb(List<QrCode> qrCodes)
+        {
+            try
+            {
+                foreach (var qr in qrCodes)
+                {
+                    string sql =
+                        $"INSERT INTO QrCode (QrID) VALUES ('{qr.QrId}')";
+
+                    DbWrapper.Wrapper.RunNonQuery(sql);
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in SaveQRtoDb: {ex.Message}");
+                return false;
+            }
+        }
+
+
+    }
+}
