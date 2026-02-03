60f05b57621b8dd521e0226ae724d73a74a54111
Jeremias Holzinger <J.Holzinger@htlvb.at>
2026-02-03 18:53:41 +0100

    Edit IsValidAdmin()

--- ClassLibrary/AdminService.cs
+++ ClassLibrary/AdminService.cs
@@ -6,13 +6,17 @@ namespace ClassLibrary
     {
         public static bool IsValidAdmin(string email, string password)
         {
-            DbWrapperMySqlV2 db = DbWrapperMySqlV2.Wrapper;
-
-            string sql = $"SELECT * FROM foa_admins WHERE email = '{email}' AND password_hash = '{password}'";
-
-            DataTable result = db.RunQuery(sql);
-
-            return result.Rows.Count > 0;
+            try
+            {
+                DbWrapperMySqlV2 db = DbWrapperMySqlV2.Wrapper;
+                string sql = $"SELECT * FROM foa_admins WHERE email = '{email}' AND password_hash = '{password}'";
+                DataTable result = db.RunQuery(sql);
+                return result.Rows.Count > 0;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
     }
 }
