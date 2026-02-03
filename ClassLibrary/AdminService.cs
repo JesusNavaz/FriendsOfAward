e1848af0a45181571ac0d1d65d1a9db8dc5220be
jholz932 <Jeremias.Holzingersm@gmail.com>
2026-02-03 12:21:13 +0100

    AdminService.cs erstellen in ClassLibrary, IsValidAdmin() Methode in AdminService

---
+++ ClassLibrary/AdminService.cs
@@ -0,0 +1,18 @@
+﻿using System.Data;
+
+namespace ClassLibrary
+{
+    public class AdminService
+    {
+        public static bool IsValidAdmin(string email, string password)
+        {
+            DbWrapperMySqlV2 db = DbWrapperMySqlV2.Wrapper;
+
+            string sql = $"SELECT * FROM foa_admins WHERE email = '{email}' AND password = '{password}'";
+
+            DataTable result = db.RunQuery(sql);
+
+            return result.Rows.Count > 0;
+        }
+    }
+}
